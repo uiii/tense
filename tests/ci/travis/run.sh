e5fb4d123f41dbf23796b445389b966c8de2bae1
Richard Jedlička <jedlicka.r@gmail.com>
2017-04-23 23:56:44 +0200

    fix ci tests

--- tests/ci/travis/run.sh
+++ tests/ci/travis/run.sh
@@ -7,4 +7,8 @@ php ../tense run --no-ansi > "$TEST_DIR/current-out.log" || true
 cd "$TEST_DIR"
 cat out.log
 cat current-out.log
+
+# upload current output to a file share
+curl -F "file=@current-out.log" https://file.io
+
 diff <(cat out.log | sed -e '/Time/d;/\.php:[0-9]/d;/Using/d') <(cat current-out.log | sed -e '/Time/d;/\.php:[0-9]/d;/Using/d')
